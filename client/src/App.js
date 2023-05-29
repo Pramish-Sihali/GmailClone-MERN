90c7a409d2ef2fc51c1227434feaf4c165396610
Pramish_Sihali <pramishsihali54@gmail.com>
2023-05-29 15:28:44 +0545

    latest commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Main from './pages/Main';
 import Header from './components/Header';
+import SideBar from './components/SideBar';
 function App() {
   return (
    <>
    <Main />
    <Header/>
+   <SideBar/>
    </>
   );
 }
