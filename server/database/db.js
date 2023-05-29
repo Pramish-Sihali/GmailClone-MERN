57f4aabd314ba0883838dce8f83ba430ab7f34b8
Pramish_Sihali <pramishsihali54@gmail.com>
2023-05-29 17:59:16 +0545

    latest commit

--- server/database/db.js
+++ server/database/db.js
@@ -7,7 +7,7 @@ const USERNAME = process.env.DB_USERNAME;
 const PASSWORD = process.env.DB_PASSWORD; 
 
 const Connection = () => {
-    const DB_URI = `mongodb://${USERNAME}:${PASSWORD}@ac-ppg953u-shard-00-00.tmltqdy.mongodb.net:27017,ac-ppg953u-shard-00-01.tmltqdy.mongodb.net:27017,ac-ppg953u-shard-00-02.tmltqdy.mongodb.net:27017/?ssl=true&replicaSet=atlas-ac66g8-shard-0&authSource=admin&retryWrites=true&w=majority`;
+    const DB_URI = `mongodb+srv://${USERNAME}:${PASSWORD}@gmail-101.tmltqdy.mongodb.net/?retryWrites=true&w=majority`;
     try {
         mongoose.connect(DB_URI, { useNewUrlParser: true });
         mongoose.set('strictQuery', false);
