62a83efe46f74d0cf41abb6e557bf970cabd4cb6
Pramish_Sihali <pramishsihali54@gmail.com>
2023-05-29 17:32:02 +0545

    latest commit

--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -1,6 +1,6 @@
 import { useState, Suspense } from 'react';
-
-import { Header, SideBar } from '../components';
+import Header from '../components/Header';
+import SideBar from '../components/SideBar';
 import { Box, styled } from '@mui/material';
 import { Outlet } from 'react-router-dom';
 import SuspenseLoader from '../components/common/SuspenseLoader';
