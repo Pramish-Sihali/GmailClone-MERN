4533429ece216ba4368a5cc585f563314f54f8b4
Pramish_Sihali <pramishsihali54@gmail.com>
2023-05-29 15:14:44 +0545

    latest commit

---
+++ client/src/pages/Main.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const propTypes = {};
+
+const defaultProps = {};
+
+
+const Main = () => {
+    return <div></div>;
+}
+
+Main.propTypes = propTypes;
+Main.defaultProps = defaultProps;
+
+
+export default Main;
